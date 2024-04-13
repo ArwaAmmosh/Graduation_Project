92e2ab0269a4176649bbde9d9a49a542565981c2
Nermeenelmasry <112644208+nermeenelmasry286@users.noreply.github.com>
2024-04-13 22:58:44 +0200

    toolcontroller

---
+++ Controllers/ToolsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Graduation_Project.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ToolsController : ControllerBase
+        {
+            private readonly UNITOOLDbContext _context;
+
+            public ToolsController(UNITOOLDbContext context)
+            {
+                _context = context;
+            }
+
+            // POST: api/Tool
+            [HttpPost]
+            public async Task<ActionResult<Tool>> PostTool(Tool tool)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                // Add the tool to the database
+                _context.Tools.Add(tool);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetTool", new { id = tool.ToolId }, tool);
+            }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tool>>> GetTools()
+        {
+            var tools = await _context.Tools.ToListAsync();
+            return tools;
+        }
+
+        // GET: api/Tool/category/{category}
+        [HttpGet("category/{category}")]
+            public async Task<ActionResult<IEnumerable<Tool>>> GetToolsByCategory(string category)
+            {
+                var tools = await _context.Tools
+                    .Where(tool => tool.Category == category)
+                    .ToListAsync();
+
+                if (tools == null || !tools.Any())
+                {
+                    return NotFound(); // Return 404 if no tools found for the category
+                }
+
+                return tools;
+            }
+        }
+}
+
--- Features/User/commands/Handlers/UserCommandHandler.cs
+++ Features/User/commands/Handlers/UserCommandHandler.cs
@@ -1,4 +1,4 @@
-﻿using Azure;
+﻿﻿using Azure;
 using Graduation_Project.Bases;
 using Graduation_Project.Features.User.commands.Models;
 using MediatR;
@@ -6,7 +6,7 @@ using NuGet.Protocol.Plugins;
 
 namespace Graduation_Project.Features.User.commands.Handlers
 {
-    public class UserCommandHandler : ResponseHandler,IRequestHandler<AddUserCommand,Response<string>>
+    public class UserCommandHandler 
     {
         #region Fields
         #endregion
