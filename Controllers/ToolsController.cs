16d98e682d9a459dd40203a0ca0c57eae0f05d2c
Nermeenelmasry <112644208+nermeenelmasry286@users.noreply.github.com>
2024-04-14 01:50:20 +0200

    EditOnToolController

--- Controllers/ToolsController.cs
+++ Controllers/ToolsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Graduation_Project.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Graduation_Project.Controllers
 {
@@ -13,35 +14,43 @@ namespace Graduation_Project.Controllers
                 _context = context;
             }
 
-            // POST: api/Tool
-            [HttpPost]
-            public async Task<ActionResult<Tool>> PostTool(Tool tool)
-            {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
+        
 
-                // Add the tool to the database
-                _context.Tools.Add(tool);
-                await _context.SaveChangesAsync();
-
-                return CreatedAtAction("GetTool", new { id = tool.ToolId }, tool);
-            }
-
+        // GET: api/Tool
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Tool>>> GetTools()
+        public async Task<ActionResult<IEnumerable<ToolDto>>> GetTools()
         {
-            var tools = await _context.Tools.ToListAsync();
+            var tools = await _context.Tools
+        .Select(t => new ToolDto
+        {
+            Name = t.Name,
+            Description = t.Description,
+            RentTime = t.RentTime,
+            Price = t.Price,
+            College = t.College,
+            University = t.University
+        })
+        .ToListAsync();
+
             return tools;
+
         }
 
         // GET: api/Tool/category/{category}
         [HttpGet("category/{category}")]
-            public async Task<ActionResult<IEnumerable<Tool>>> GetToolsByCategory(string category)
+            public async Task<ActionResult<IEnumerable<ToolDto>>> GetToolsByCategory(string category)
             {
                 var tools = await _context.Tools
                     .Where(tool => tool.Category == category)
+                    .Select(t => new ToolDto
+                    {
+                        Name = t.Name,
+                        Description = t.Description,
+                        RentTime = t.RentTime,
+                        Price = t.Price,
+                        College = t.College,
+                        University = t.University
+                    })
                     .ToListAsync();
 
                 if (tools == null || !tools.Any())
@@ -51,6 +60,46 @@ namespace Graduation_Project.Controllers
 
                 return tools;
             }
+        // GET: api/Tool/category/{category}
+        [HttpGet("University/{University}")]
+        public async Task<ActionResult<IEnumerable<ToolDto>>> GetToolsByUnivserity(string University)
+        {
+            var tools = await _context.Tools
+                .Where(tool => tool.University == University)
+                .Select(t => new ToolDto
+                {
+                    Name = t.Name,
+                    Description = t.Description,
+                    RentTime = t.RentTime,
+                    Price = t.Price,
+                    College = t.College,
+                    University = t.University
+                })
+                .ToListAsync();
+
+            if (tools == null || !tools.Any())
+            {
+                return NotFound(); // Return 404 if no tools found for the category
+            }
+
+            return tools;
         }
+
+        // POST: api/Tool
+        [HttpPost]
+        public async Task<ActionResult<Tool>> PostTool(Tool tool)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Add the tool to the database
+            _context.Tools.Add(tool);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTool", new { id = tool.ToolId }, tool);
+        }
+    }
 }
 
--- Dtos/ToolDto.cs
+++ Dtos/ToolDto.cs
@@ -2,11 +2,13 @@
 {
     public class ToolDto
     {
-        public int ToolId { get; set; }
+       
         public string Name { get; set; }
         public string Description { get; set; }
         public string RentTime { get; set; }
         public string College { get; set; }
         public string University { get; set; }
+
+        public int Price { get; set; }
     }
 }
